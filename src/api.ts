8e1fdbb9abd25982e3fd2f2a1904714635f29705
Артем Антонянц <aa.artem.0303@mail.ru>
2025-07-17 19:35:10 +0500

    chore: add token

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const GITHUB_TOKEN = "";
-
 export const api = createApi({
   reducerPath: "githubApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.github.com/graphql",
     method: "POST",
     prepareHeaders: (headers) => {
-      headers.set("Authorization", `Bearer ${GITHUB_TOKEN}`);
+      headers.set("Authorization", `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`);
       headers.set("Content-Type", "application/json");
       return headers;
     },
