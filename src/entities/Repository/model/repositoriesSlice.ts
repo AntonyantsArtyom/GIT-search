5b27d7e7e648c55cfd9cfd3d566e0a3815b3f837
Артем Антонянц <aa.artem.0303@mail.ru>
2025-07-18 09:08:22 +0500

    docs: add jsdoc

--- src/entities/Repository/model/repositoriesSlice.ts
+++ src/entities/Repository/model/repositoriesSlice.ts
@@ -24,6 +24,11 @@ export const repositoriesSlice = createSlice({
   name: "repositories",
   initialState,
   reducers: {
+    /**
+     * Sets the list of repositories, the total count of repositories and the page info
+     *
+     * @param {{ repositories: Repository[]; repositoryCount: number; pageInfo: { hasNextPage: boolean; hasPreviousPage: boolean; endCursor: string | null; startCursor: string | null; }; }} payload - the data to set
+     */
     setRepositories: (
       state,
       action: PayloadAction<{
@@ -42,22 +47,54 @@ export const repositoriesSlice = createSlice({
       state.pageInfo = action.payload.pageInfo;
     },
 
+    /**
+     * Sets the number of records to display per page
+     *
+     * @param {number} payload - the number of records per page
+     */
+
     setRecordsPerPage: (state, action: PayloadAction<number>) => {
       state.recordsPerPage = action.payload;
     },
+    /**
+     * Sets the name of the repository to search for
+     *
+     * @param {string} payload - the name of the repository to search for
+     */
     setRepositoryName: (state, action: PayloadAction<string>) => {
       state.repositoryName = action.payload;
     },
+    /**
+     * Sets the id of the repository for which to fetch the details
+     *
+     * @param {string | null} payload - the id of the repository to fetch the details or null to reset the state
+     */
     setRepositoryDetailsId: (state, action: PayloadAction<string | null>) => {
       state.repositoryDetailsId = action.payload;
     },
+    /**
+     * Sets the current page and the direction of the pagination
+     *
+     * @param {number} payload - the page number to set
+     */
     setCurrentPageAndDirection: (state, action: PayloadAction<number>) => {
       state.paginationDirection = state.currentPage > action.payload ? "backward" : "forward";
       state.currentPage = action.payload;
     },
+    /**
+     * Sets the order direction for sorting repositories
+     *
+     * @param {"asc" | "desc"} payload - the sorting order direction
+     */
+
     setOrder: (state, action: PayloadAction<"asc" | "desc">) => {
       state.order = action.payload;
     },
+    /**
+     * Sets the field for sorting repositories
+     *
+     * @param {"stars" | "forks" | "updated"} payload - the field to sort by
+     */
     setOrderField: (state, action: PayloadAction<"stars" | "forks" | "updated">) => {
       state.orderBy = action.payload;
     },
--- src/entities/Repository/types/repositiry.types.ts
+++ src/entities/Repository/types/repositiry.types.ts
@@ -1,3 +1,15 @@
+/**
+ * Repository type
+ *
+ * @prop {string} id - repository id
+ * @prop {string} name - repository name
+ * @prop {number} stargazerCount - number of stars
+ * @prop {number} forkCount - number of forks
+ * @prop {string} updatedAt - updated date
+ * @prop {string} [description] - repository description
+ * @prop {{ name: string }} [licenseInfo] - license information
+ * @prop {{ name: string }} [primaryLanguage] - primary language
+ */
 export type Repository = {
   id: string;
   name: string;
@@ -9,6 +21,16 @@ export type Repository = {
   primaryLanguage?: { name: string } | null;
 };
 
+/**
+ * Repository details type
+ *
+ * @prop {string} id - repository id
+ * @prop {string} name - repository name
+ * @prop {number} stargazerCount - number of stars
+ * @prop {{ name: string }} [primaryLanguage] - primary language
+ * @prop {{ name: string }[]} languages - list of languages
+ * @prop {{ name: string; spdxId: string }} [licenseInfo] - license information
+ */
 export type RepositoryDetails = {
   id: string;
   name: string;
@@ -18,6 +40,21 @@ export type RepositoryDetails = {
   licenseInfo?: { name: string; spdxId: string } | null;
 };
 
+/**
+ * State type
+ *
+ * @prop {number} recordsPerPage - number of records per page
+ * @prop {"stars" | "forks" | "updated"} [orderBy] - field to sort by
+ * @prop {"desc" | "asc"} order - order of sorting
+ * @prop {number} currentPage - current page
+ * @prop {"forward" | "backward"} paginationDirection - direction of pagination
+ * @prop {string} repositoryName - repository name for search
+ * @prop {boolean} isLoading - is loading flag
+ * @prop {Repository[]} repositories - list of repositories
+ * @prop {number} repositoryCount - total number of repositories
+ * @prop {string | null | undefined} repositoryDetailsId - repository id for which to load detailed information
+ * @prop {{ hasNextPage: boolean; hasPreviousPage: boolean; startCursor: string | null; endCursor: string | null }} pageInfo - information about the page
+ */
 export interface RepositoriesState {
   recordsPerPage: number;
   orderBy?: "stars" | "forks" | "updated";
@@ -28,7 +65,7 @@ export interface RepositoriesState {
   isLoading: boolean;
   repositories: Repository[];
   repositoryCount: number;
-  repositoryDetailsId?: string | null;
+  repositoryDetailsId?: string | null | undefined;
   pageInfo: {
     hasNextPage: boolean;
     hasPreviousPage: boolean;
@@ -37,6 +74,16 @@ export interface RepositoriesState {
   };
 }
 
+/**
+ * Repository search node type
+ *
+ * @prop {string} id - repository id
+ * @prop {string} name - repository name
+ * @prop {number} stargazerCount - number of stars
+ * @prop {string} [language] - language
+ * @prop {number} forkCount - number of forks
+ * @prop {string} updatedAt - updated date
+ */
 export interface RepositorySearchNode {
   node: {
     id: string;
@@ -48,6 +95,13 @@ export interface RepositorySearchNode {
   };
 }
 
+/**
+ * Search repositories response type
+ *
+ * @prop {number} repositoryCount - total number of repositories
+ * @prop {{ startCursor: string | null; endCursor: string | null; hasNextPage: boolean; hasPreviousPage: boolean }} pageInfo - information about the page
+ * @prop {RepositorySearchNode[]} edges - list of nodes, each containing information about a repository
+ */
 export interface SearchRepositoriesResponse {
   data: {
     search: {
@@ -63,6 +117,11 @@ export interface SearchRepositoriesResponse {
   };
 }
 
+/**
+ * Get repository by id response type
+ *
+ * @prop {{ name: string; stargazerCount: number; primaryLanguage?: { name: string } | null; languages: { nodes: { name: string }[] }; licenseInfo?: { name: string } | null }} node - repository information
+ */
 export interface GetRepositoryByIdResponse {
   data: {
     node: {
