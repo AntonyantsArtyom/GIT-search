2aaad5a6a0fd18c203cd0adefd0856ff1da89efa
Артем Антонянц <aa.artem.0303@mail.ru>
2025-07-18 07:06:39 +0500

    chore: finish UI of additional info

--- src/entities/Repository/UI/RepositoryFullInfo.tsx
+++ src/entities/Repository/UI/RepositoryFullInfo.tsx
@@ -5,6 +5,8 @@ import type { RootState } from "../../../store";
 import { useGetRepositoryByIdQuery } from "../api/RepositoryQueries";
 import { LoadingMessage } from "../../../shared/LoadingMessage/HelloMessage";
 import { useMemo } from "react";
+import StarIcon from "@mui/icons-material/Star";
+import { Box, Chip, Typography } from "@mui/material";
 
 export const RepositoryFullInfo = () => {
   const { repositoryDetailsId } = useSelector((state: RootState) => state.repositories);
@@ -21,15 +23,24 @@ export const RepositoryFullInfo = () => {
     }
 
     return (
-      <>
-        <h2>{repository.name}</h2>
-        <p>⭐ {repository.stargazerCount}</p>
-        <p>{repository.primaryLanguage?.name || "—"}</p>
-        <p>{repository.languages?.nodes.map((lang: any) => lang.name).join(", ") || "—"}</p>
-        <p>{repository.licenseInfo?.name || "—"}</p>
-      </>
+      <Box className={styles.information}>
+        <Typography variant="h4">{repository.name}</Typography>
+        <Box className={styles.starsAndLanguage}>
+          <Chip className={styles.mainLanguage} color="primary" label={repository.primaryLanguage?.name || "—"} variant="outlined" />
+          <Box className={styles.stars}>
+            <StarIcon />
+            <Typography variant="body1">{repository.stargazerCount}</Typography>
+          </Box>
+        </Box>
+        <Box className={styles.languages}>
+          {repository.languages?.nodes.map((lang: any) => (
+            <Chip label={lang.name} variant="outlined" />
+          ))}
+        </Box>
+        <Typography variant="body1">{repository.licenseInfo?.name || "—"}</Typography>
+      </Box>
     );
   }, [isFetching, repositoryDetailsId]);
 
-  return <div className={styles.fullinfo}>{BlockForRender}</div>;
+  return <div className={styles.additionalBlock}>{BlockForRender}</div>;
 };
